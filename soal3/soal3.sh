ea169773b48e6e42eb8732bc016fc639649722b4
atitiandes <55976812+atitiandes@users.noreply.github.com>
2020-02-21 17:32:22 +0700

    Create soal3.sh

---
+++ soal3/soal3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+count=1
+
+while read p; do
+for ((i=1; i<=2; i=i+1))
+do
+
+        wget -O "pdkt_kusuma_${count}.jpg" $p -o wget.log
+                count=$((count+1))
+
+       done
+done 
+
