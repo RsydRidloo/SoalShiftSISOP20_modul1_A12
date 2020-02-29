3f3ee2d176e01cc97e30c63af71bd00892ef9ecf
atitiandes <55976812+atitiandes@users.noreply.github.com>
2020-02-29 09:45:35 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -6,7 +6,7 @@ while read p; do
 for ((i=1; i<=28; i=i+1))
 do
 
-        wget -O "pdkt_kusuma_${count}.jpg" $p -o wget.log
+        wget -O "pdkt_kusuma_${count}.jpg" $p -a wget.log
                 count=$((count+1))
 
        done
