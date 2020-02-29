85006f37fce3d20684f9643e228e805eb272a9b8
TropEX <61292802+TropEX@users.noreply.github.com>
2020-02-29 19:20:15 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+
+for var in $@;do
+
+touch waktu.txt
+if [[ $var =~ ^[a-zA-Z]+$ ]];
+then
+
 flag=1
-while [[ $flag == 1 ]]
+while [ $flag -eq 1 ]
 do
-read -p "Masukan hanya alphabet saja:" var
-if [[ ! $var =~ ^[[a-zA-Z]]+$ ]]
+randompswrd=$(cat /dev/urandom|tr -dc A-Za-z0-9 | head -c 28)
+if [[ "$randompswrd" =~ [A-Z] ]] && [[ "$randompswrd" =~ [a-z] ]] && [[ "$randompswrd" =~ [0-9] ]]
 then
 flag=0
+fi
+done
 
-randompswrd=$(head /dev/urandom|tr -dc A-Za-z0-9 |head -c 28 )
 echo "$randompswrd"  > "$var".txt
+
 
 else
         echo "Salah"
-fi
-done
+        
