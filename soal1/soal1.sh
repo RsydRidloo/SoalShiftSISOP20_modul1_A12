13b805633f943907cb650d8c44f9f8180b590e1b
TropEX <61292802+TropEX@users.noreply.github.com>
2020-02-29 19:21:20 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 echo "A. Region dengan Profit Sedikit:"
-awk -F '        '  '{arr[$13]+=$21}
+
+region=$(awk -F '\t'  '{arr[$13]+=$21}
 END {
         for (hasil in arr) {print hasil}
-}' Sample-Superstore.tsv | sort -g | head -1
+}' Sample-Superstore.tsv | sort -g | head -1)
+echo "$region"
+
 
 echo " "
 echo "B. 2 Negara dengan Profit Sedikit:"
-awk -F '        '  '$13 ~/Central/ {arr[$11]+=$21}
+
+state=$(awk -F '\t' -v a=$region '{if($13==a)arr[$11]+=$21}
 END {
         for(hasil in arr) {print arr[hasil]"  "hasil}
-}' Sample-Superstore.tsv | sort -g | head -2
+}' Sample-Superstore.tsv | sort -g | head -2)
+echo "$state"
 
 echo " "
 echo "C. 10 Produk yang memiliki Profit paling sedikit berdasarkan 2 negara: "
-awk -F '        '  '$13 ~/Central/{
-        if ($11 == "Texas" || $11 == "Illinois") arr[$17]+=$21
+
+awk -F '\t' -v a=$region '{if($11 == "Texas" || $11 == "Illinois")arr[$17]+=$21
 }
 END {
         for(hasil in arr) {print arr[hasil]" "hasil}
